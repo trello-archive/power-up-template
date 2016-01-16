dc8c94f4883e15eef32c8a6dde1decf9a2fea8c6
Matt Cowan <mattcowan@outlook.com>
2016-01-16 16:36:27 -0500

    Fix badge icon colors and update refresh interval
    Use a white icon for colored badges. Update the refresh interval to the
    minimum (10 seconds) to make the dynamic badges more obvious.

--- js/template.js
+++ js/template.js
@@ -1,17 +1,24 @@
 /* global TrelloPowerUp */
 
+var WHITE_ICON = './images/icon-white.svg';
+var GRAY_ICON = './images/icon-gray.svg';
+
 var getBadges = function(t, includeTitle){
   return t.card('name')
   .get('name')
   .then(function(cardName){
     var badgeColor;
+    var icon = GRAY_ICON;
     var lowercaseName = cardName.toLowerCase();
     if(lowercaseName.indexOf('green') > -1){
       badgeColor = 'green';
+      icon = WHITE_ICON;
     } else if(lowercaseName.indexOf('yellow') > -1){
       badgeColor = 'yellow';
+      icon = WHITE_ICON;
     } else if(lowercaseName.indexOf('red') > -1){
       badgeColor = 'red';
+      icon = WHITE_ICON;
     }
     
     if(lowercaseName.indexOf('dynamic') > -1){
@@ -19,10 +26,10 @@ var getBadges = function(t, includeTitle){
         dynamic: function(){
           return {
             title: includeTitle ? 'Detail Badge' : null,
-            text: 'Dynamic ' + (Math.random() * 10).toFixed(0).toString(),
-            icon: './images/icon.svg',
+            text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
+            icon: icon,
             color: badgeColor,
-            refresh: 30
+            refresh: 10
           }
         }
       }]
@@ -31,7 +38,7 @@ var getBadges = function(t, includeTitle){
     return [{
       title: includeTitle ? 'Detail Badge' : null,
       text: 'Static',
-      icon: './images/icon.svg',
+      icon: icon,
       color: badgeColor
     }];
   })
