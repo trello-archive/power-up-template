f627925f4d1a2af5e055fd3a7d4156bf8e083f6e
Matt Cowan <mattcowan@outlook.com>
2016-01-17 21:48:35 -0500

    Implement attachment-sections capability
    Show an attachment section if we see any Yellowstone urls attached.

---
+++ js/section.js
@@ -0,0 +1,13 @@
+/* global TrelloPowerUp */
+
+var t = TrelloPowerUp.iframe();
+
+// you can access arguments passed to your iframe like so
+var arg = t.arg('arg');
+
+t.render(function(){
+  // make sure your rendering logic lives here, since we will
+  // recall this method as the user adds and removes attachments
+  // from your section
+  t.sizeTo('#content');
+});
--- js/template.js
+++ js/template.js
@@ -133,7 +133,36 @@ var cardButtonCallback = function(t){
 
 TrelloPowerUp.initialize({
   'attachment-sections': function(t, options){
-    return [];
+    // options.entries is a list of the attachments for this card
+    // you can look through them and 'claim' any that you want to
+    // include in your section.
+    
+    // we will just claim urls for Yellowstone
+    var claimed = options.entries.filter(function(attachment){
+      return attachment.url.indexOf('http://www.nps.gov/yell/') == 0;
+    });
+    
+    // you can have more than one attachment section on a card
+    // you can group items together into one section, have a section
+    // per attachment, or anything in between.
+    if(claimed && claimed.length > 0){
+      // if the title for your section requires a network call or other
+      // potentially length operation you can provide a function for the title
+      // that returns the section title. If you do so, provide a unique id for
+      // your section
+      return [{
+        id: 'Yellowstone', // optional if you aren't using a function for the title
+        claimed: claimed,
+        icon: GRAY_ICON,
+        title: 'Example Attachment Section: Yellowstone',
+        content: {
+          type: 'iframe',
+          url: t.signUrl('./section.html', { arg: 'you can pass your section args here' })
+        }
+      }];
+    } else {
+      return [];
+    }
   },
   'attachment-thumbnail': function(t, options){
     var parkName = formatNPSUrl(t, options.url);
